3da5240c18b9d8c0fd6eba3d6aa8593229bce20e
OleksandrUtkin <32433604+OleksandrUtkin@users.noreply.github.com>
2019-07-06 01:59:29 +0300

    Update task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,7 +1,7 @@
-const a = +prompt('Please, enter value of triangle side a', "");
-const b = +prompt('Please, enter value of triangle side b', "");
-const c = +prompt('Please, enter value of triangle side c', "");
-if (isNaN(a) || isNaN(b) || isNaN(c) ) {
+const a = +prompt('Please, enter value of triangle side a', '');
+const b = +prompt('Please, enter value of triangle side b', '');
+const c = +prompt('Please, enter value of triangle side c', '');
+if (!isFinite(a) || !isFinite(b) || !isFinite(c) ) {
 	alert('Please, enter only numeric value')
 } else if (a+b < c || a+c < b || c+b < a || a<=0 || b<=0 || c<=0) {
 	console.log('Triangle doesn’t exist')
