f651f34b84bfea4a7efc4b62f1d9e9111b214cbc
OleksandrUtkin <32433604+OleksandrUtkin@users.noreply.github.com>
2019-07-06 00:35:44 +0300

    Update task1.js

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -4,10 +4,19 @@ const bx = +prompt('Please, input numeric value bx', '');
 const by = +prompt('Please, input numeric value by', '');
 const cx = +prompt('Please, input numeric value cx', '');
 const cy = +prompt('Please, input numeric value cy', '');
-let xc = (ax+bx)/2 ;
-let yc = (ay+by)/2 ;
-if (xc===cx && yc===cy) {
-	console.log(true) 
+if (ax===bx && ay===by) {
+	console.log('A(ax,ay) with B(bx,by) not a line, please, try again')
+} else if (!isFinite(ax) || !isFinite(ay) || !isFinite(bx) || !isFinite(by) || !isFinite(cx) || !isFinite(cy) ) {
+	console.log('please, try again with numbers')
 } else {
-	console.log(false)
+	const magicNumber = 2;
+	let xc = (ax+bx)/magicNumber;
+	let yc = (ay+by)/magicNumber;
+	if (xc===cx && yc===cy) {
+		console.log(true) 
+	} else {
+		console.log(false)
+	}
 }
+
+
