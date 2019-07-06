f4f7ede36d0160e32cb36ce32c6646777e8ef70a
OleksandrUtkin <32433604+OleksandrUtkin@users.noreply.github.com>
2019-07-07 01:44:00 +0300

    Update task2.js

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,74 @@
-// Your code goes here
+let totalPrize = 0,
+	possiblePrize = 100,
+	randomTo = 8,
+	attempts = 3,
+	letsPlay = confirm('Do you want to play a game?');
+	if (letsPlay === false) {
+		alert('You did not become a billionaire, but can.')
+	}
+while (letsPlay === true) {
+	let randomFrom = 0,
+		random = Math.floor(Math.random() * (randomTo - randomFrom + 1)) + randomFrom,
+		yourNumber = +prompt(`Choose a roulette pocket number from 0 to ${randomTo} 
+		Attempts left: ${attempts} 
+		Total prize:  ${totalPrize}$ 
+		Possible prize on current attempt: ${possiblePrize}$`)
+		if (yourNumber === random) {
+			attempts = 3;
+			totalPrize += possiblePrize;
+			randomTo +=4;
+			letsPlay = confirm(`Congratulation, you won!   
+			Your prize is: ${possiblePrize}$. Do you want to continue?`);
+			possiblePrize *=2;
+			if (letsPlay === false) {
+				alert(`Thank you for your participation. Your prize is: ${totalPrize}$`)
+				letsPlay = confirm('Want to play again?')
+				if(letsPlay === true) {
+					totalPrize = 0,
+					possiblePrize = 100,
+					randomTo = 8,
+					attempts = 3
+				}
+			}
+		} else {
+			let possiblePrize2 = possiblePrize;
+			while(yourNumber !== random) {
+				possiblePrize2 /=2;	
+				attempts--; 
+				if (attempts === 0) {
+					letsPlay = confirm('You lose, want to play again?');
+					totalPrize = 0,
+					possiblePrize = 100,
+					randomTo = 8,
+					attempts = 3
+					break
+				} else {
+					yourNumber = +prompt(`Choose a roulette pocket number from 0 to ${randomTo} 
+					Attempts left: ${attempts} 
+					Total prize:  ${totalPrize}$ 
+					Possible prize on current attempt: ${possiblePrize2}$`) 
+				}
+			}
+			if (yourNumber === random) {
+				attempts = 3;
+				totalPrize += possiblePrize2;
+				randomTo +=4;
+				letsPlay = confirm(`Congratulation, you won!   
+				Your prize is: ${possiblePrize2}$. Do you want to continue?`);
+				possiblePrize *=2;
+				if (letsPlay === false) {
+					alert(`Thank you for your participation. Your prize is: ${totalPrize}$`)
+					letsPlay = confirm('Want to play again?')
+					if(letsPlay === true) {
+						totalPrize = 0,
+						possiblePrize = 100,
+						randomTo = 8,
+						attempts = 3
+					}
+				}
+			}
+		}
+} 
+
+	
+
